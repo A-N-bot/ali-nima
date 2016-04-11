8295dc0996d7150b8c13402ec4fabbf6c482b293
Nima <anonymou3team@gmail.com>
2016-04-11 06:42:09 +0430

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -15,7 +15,7 @@ if msg.text == "Salam" then
   return "Salam aleykom"
 end
 if msg.text == "salam" then
-  return "va aleykol asalam"
+  return "va aleykomo salam"
 end
 if msg.text == "zac" then
   return "Nagaeedim"
@@ -23,13 +23,13 @@ end
 if msg.text == "Zac" then
   return "Nagaeedim"
 end
-if msg.text == "Sbss" then
+if msg.text == "anonymous" then
   return "Is the best"
 end
-if msg.text == "Sbss" then
+if msg.text == "Anonymous" then
   return "Is the best"
 end
-if msg.text == "SBSS" then
+if msg.text == "anonymous" then
   return "Jnm?"
 end
 if msg.text == "bot" then
@@ -47,6 +47,32 @@ end
 if msg.text == "سلام" then
   return "علیـک"
 end
+if msg.text == "خوبی" then
+  return "ممنون شما خوبی؟"
+end
+if msg.text == "چه خبر" then
+  return "سلامتیت عزیز تو چه خبر؟"
+end
+if msg.text == "چیکارمیکنی" then
+  return "والله حقیقتش هیچی"
+end
+if msg.text == "انونیموس" then
+  return "کونده چیکار بابام داری؟"
+end
+if msg.text == "نیما" then
+  return "چیکار بابام داری؟بعدشم نیماجوون نه نیما کسکش"
+end
+if msg.text == "نیماجوون" then
+  return "افرین اینجوری بگو ولی ناموسن کیرم بلندشده"
+end
+if msg.text == "به کیرم" then
+  return "کیرت تو کونت کسکش"
+end
+if msg.text == "bk" then
+  return "یه دفعه دیگه گفتی به کیرم گلوبال بن میشی"
+if msg.text == "وااااخ" then
+  return "اینجوری نگو واخ کیرم پا میشه"
+end
 if msg.text == "slm" then
   return "سلام"
 end
@@ -59,10 +85,10 @@ end
 if msg.text == "خدافظ" then
   return "Bye Bye"
 end
-if msg.text == "Mr Ahs" then
+if msg.text == "نیما" then
   return "چیکار داری بابامو؟"
 end
-if msg.text == "@Mr_Ah_s" then
+if msg.text == "@anonymou3nk" then
   return "چیکار داری بابامو؟"
 end
 end
@@ -79,13 +105,21 @@ return {
     "^[Bb]ye$",
     "^?$",
     "^[Ss]alam$",
-    "^@Mr_Ah_S$",
-    "^Mr Ahs$",
+    "^$نیما",
+    "^@anonymou3nk$",
     "^خدافظ$",
     "^بای$",
     "^[Ss]lm$",
     "^SBSS$",
     "^[Ss]bss$",
+    "^خوبی$",
+    "^چه خبر$",
+    "^چیکارمیکنی$",
+    "^انونیموس$",
+    "^نیماجوون$",
+    "^bk$",
+    "^به کیرم$",
+    "^وااااخ$",
     }, 
   run = run,
   pre_process = pre_process
